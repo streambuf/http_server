94e5177d6dbc8750191fe3b4bcc4ceabdff42554
Moiseev Maxim (streambuf) <streambuf@mail.ru>
2015-02-20 23:38:33 +0300

    ExecutorTaskService was added

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ all: compile
 clean:
 	@-rm -rf main
 compile: clean
-	@javac -d compile -classpath lib/jcommander-1.30.jar src/httpd/java/**/*.java src/httpd/java/*.java
+	@javac -d . -classpath lib/jcommander-1.30.jar src/httpd/java/**/*.java src/httpd/java/*.java
+	@jar -cvfm httpd.jar MANIFEST.MF *.class **/*.class
 	@echo -e 'Success!'
--- src/httpd/java/Server.java
+++ src/httpd/java/Server.java
@@ -1,21 +1,35 @@
 import com.beust.jcommander.JCommander;
 import utils.CommandLineParser;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by max on 19.02.15.
  */
 public class Server {
+    private static String rootDir;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+
         configureServer(args);
+        ExecutorTaskService threadPool = new ExecutorTaskService(20);
+        ServerSocket ss = new ServerSocket(8080);
+
+        while (true) {
+            Socket s = ss.accept();
+            threadPool.execute(new HandlerSocket(s, rootDir));
+        }
+
     }
 
     private static void configureServer(String[] args) {
         CommandLineParser jct = new CommandLineParser();
         try {
             new JCommander(jct, args);
+            rootDir = jct.getRootDir();
             System.out.println("NCPU: " + jct.getNumCPU());
-            System.out.println("ROOTDIR: " + jct.getRootDir());
+            System.out.println("ROOTDIR: " + rootDir);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
